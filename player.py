6df0938fc202f9f72a28ef574c20beca98b2c7eb
Jackdau <R3lacy@gmail.com>
2018-10-30 21:37:21 -0700

    Added variables to player class

--- player.py
+++ player.py
@@ -7,10 +7,16 @@ class Player():
         self.width = 100
         self.height = 100
         self.speed = 5
-
+        self.is_left = False
+        self.is_right = False
+        self.is_jump = True
     def draw_player(self, surface, color):
         self.image = pygame.draw.rect(surface, color, ( self.x, self.y, self.width, self.height ) )
     def move_left(self):
         self.x -= self.speed
+        self.is_left = True
+        self.is_right = False
     def move_right(self):
         self.x += self.speed
+        self.is_right = True
+        self.is_left = False
